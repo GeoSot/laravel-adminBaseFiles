d89de0e1da4123e9cad23fa72d89db8f89a626f8
gsot <gsotiropoulos@elearningindustry.com>
2021-07-14 17:25:19 +0300

    change timePicker

--- resources/js/_partials/forms/fields/TimePicker.js
+++ resources/js/_partials/forms/fields/TimePicker.js
@@ -1,20 +1,20 @@
 let $;
-let instances = [];
+const instances = new Map();
 const selectors = {
     trigger: '[data-toggle="timePicker"]',
 }
 const init = () => {
     import('jquery').then(src => {
         $ = src.default
-        $(document).on('click', `${selectors.trigger} input`, function (e) {
+        $(document).on('focusin', `${selectors.trigger} input`, function (e) {
             new internalInit(this);
         });
 
-        $(function () {
-            $(selectors.trigger).each(function (e) {
-                new internalInit(this);
-            });
-        });
+        // $(function () {
+        //     $(selectors.trigger).each(function (e) {
+        //         new internalInit(this);
+        //     });
+        // });
     });
 
 }
@@ -23,17 +23,18 @@ const internalInit = function (el) {
     const _this = this;
     _this.$wrapper = ($(el).is(selectors.trigger)) ? $(el) : $(el).parents(selectors.trigger);
     _this.$hiddenInput = this.$wrapper.find('[type="hidden"]');
-    let name = _this.$hiddenInput.attr('name');
-    // if (Object.keys(BaseAdmin.forms.fields.Instances.timePickers).length && BaseAdmin.forms.fields.Instances.timePickers[name] !== 'undefined') {
-    //     return BaseAdmin.forms.fields.Instances.timePickers[name];
-    // } else {
-    instances[name] = _this;
-    // }
+    let element = _this.$hiddenInput.get(0);
+    if (instances.has(element)) {
+        return
+        // return instances.get(element)
+    } else {
+        instances.set(element, _this);
+    }
     _this.$hoursInput = _this.$wrapper.find('input').not('[type=hidden]').first();
     _this.$minutesInput = _this.$wrapper.find('input').not('[type=hidden]').last();
-    $(this.$hiddenInput).off('change.formFields');
-    $(this.$hoursInput).off('change.formFields');
-    $(this.$minutesInput).off('change.formFields');
+    $(this.$hiddenInput).off('input.formFields');
+    $(this.$hoursInput).off('input.formFields');
+    $(this.$minutesInput).off('input.formFields');
 
     _this.totalMinutes = 0;
     _this.minutes = 0;
@@ -74,12 +75,12 @@ const internalInit = function (el) {
     };
 
 
-    $(this.$hoursInput).on('change.formFields', function (e) {
+    $(this.$hoursInput).on('input.formFields', function (e) {
         _this.hours = parseInt($(this).val());
         updateTotalMinutes();
         _this.setTimeToInputs();
     });
-    $(this.$minutesInput).on('change.formFields', function (e) {
+    $(this.$minutesInput).on('input.formFields', function (e) {
         let minVal = parseInt($(this).val());
         if (minVal === 60) {
             _this.hours++;
@@ -97,7 +98,7 @@ const internalInit = function (el) {
         _this.setTimeToInputs();
     });
 
-    $(this.$hiddenInput).on('change.formFields', function (e) {
+    $(this.$hiddenInput).on('input.formFields', function (e) {
         initializeFields();
     });
     initializeFields();
