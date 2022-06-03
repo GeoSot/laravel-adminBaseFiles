15520d5903d737060bb84766339da827b3928d60
rami25 <rami@elearningindustry.com>
2022-06-03 16:46:42 +0300

    fixed bug with wrong folder path

--- filesToPublish/resources/views/vendor/laravel-log-viewer/_sideBar.blade.php
+++ filesToPublish/resources/views/vendor/laravel-log-viewer/_sideBar.blade.php
@@ -1,10 +1,11 @@
 <div class="adminSidebar navbar   h-100  bg-admin navbar-dark px-0 align-items-start">
     <section class="navbar-nav sidebar  flex-fill ">
         <ul id="menu" class="page-sidebar-menu flex-column  nav">
-            @foreach($folders as $folder)
+            @foreach($folders as $key => $fullFolder)
+                @php($folder= \Illuminate\Support\Str::afterLast($fullFolder, DIRECTORY_SEPARATOR))
                 <li class="nav-item">
                     <a href="?f={{ \Illuminate\Support\Facades\Crypt::encrypt($folder) }}"
-                       {{--data-toggle="collapse" data-target="#collapse_{{$folder}}" role="button" aria-expanded="false" aria-controls="collapse_{{$folder}}"--}}
+                       {{--                       data-toggle="collapse" data-target="#collapse_{{$folder}}" role="button" aria-expanded="false" aria-controls="collapse_{{$folder}}"--}}
                        class="px-3 nav-link d-flex align-items-center small  @if ($isActive=$current_folder == $folder) active @endif">
                         <span class="title ">{{$folder}}</span>
                         <span class="fas arrow-after ml-auto pl-2 {!! $isActive?"fa-folder-open":"fa-folder" !!}"></span>
@@ -21,8 +22,8 @@
                     </ul>
                 </li>
             @endforeach
-            @php(rsort($files))
-            @foreach($files as $file)
+            @foreach(glob($storage_path.'/*.log') as $fullFile)
+                @php($file=\Illuminate\Support\Str::afterLast($fullFile, DIRECTORY_SEPARATOR))
                 <li class="nav-item">
                     <a href="?l={{ \Illuminate\Support\Facades\Crypt::encrypt($file) }}"
                        class="small nav-link px-3 inner-level-link d-flex align-items-center @if ($current_file == $file) active @endif">
@@ -33,3 +34,12 @@
         </ul>
     </section>
 </div>
+
+
+
+
+
+
+
+
+
