43f0b889efead73f8de1406c52615ad9067cdccf
GeoSot <geo.sotis@gmail.com>
2022-11-11 01:36:58 +0200

    Make generic blade more extensible

---
+++ resources/views/admin/generic/_indexBody.blade.php
@@ -0,0 +1,13 @@
+@component($packageVariables->get('blades').'admin._components.listDataParsing',['viewVals'=>$viewVals, ]  )
+    @slot('tableData')
+        @foreach($viewVals->get('records') as $record)
+            <tr>
+                @include($packageVariables->get('blades').'admin._includes._listDataParsing_helper', ['parse'=>'firstCheckBox'])
+                @foreach($listable=$viewVals->get('extraValues')->get('listable') as $listName)
+                    @include($packageVariables->get('blades').'admin._includes._listDataParsing_helper', ['parse'=>'fields'])
+                @endforeach
+            </tr>
+        @endforeach
+    @endslot
+
+@endcomponent
--- resources/views/admin/generic/index.blade.php
+++ resources/views/admin/generic/index.blade.php
@@ -3,18 +3,5 @@
 
 @section('content')
 
-    @component($packageVariables->get('blades').'admin._components.listDataParsing',['viewVals'=>$viewVals, ]  )
-        @slot('tableData')
-            @foreach($viewVals->get('records') as $record)
-                <tr>
-                    @include($packageVariables->get('blades').'admin._includes._listDataParsing_helper', ['parse'=>'firstCheckBox'])
-                    @foreach($listable=$viewVals->get('extraValues')->get('listable') as $listName)
-                        @include($packageVariables->get('blades').'admin._includes._listDataParsing_helper', ['parse'=>'fields'])
-                    @endforeach
-                </tr>
-            @endforeach
-        @endslot
-
-
-    @endcomponent
+    @include('baseAdmin::admin.generic._indexBody')
 @endsection
